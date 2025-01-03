a0ad0182fcb13961376e6fba10faef3d2ce3f824
Florian <floriangeorges1992@gmail.com>
2025-01-03 16:32:24 +0100

    event

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
+import Button from "./Button";
+
 function App() {
-  function handleClick(e) {
-    e.stopPropagation();
-    e.preventDefault();
-    console.log("click ! : ", e);
+  function handleClick(e, from) {
+    console.log("click ! : ", e, from);
   }
 
-  function handleInput(e) {
-    console.log("input ! : ", e);
-  }
-
-  function handleFocus(e) {
-    console.log("focus ! : ", e);
-  }
-
-  function handleClickDiv(e) {
-    console.log("div click ! : ", e);
-  }
   return (
-    <div
-      onClick={handleClickDiv}
-      className=" d-flex flex-column justify-content-center align-items-center p-20"
-    >
-      <form action="">
-        <button onClick={handleClick} className="mb-20">
-          Submit
-        </button>
-      </form>
-
-      <input onFocus={handleFocus} type="text" onInput={handleInput} />
+    <div className=" d-flex flex-column justify-content-center align-items-center p-20">
+      <Button onClick={handleClick} from="App" />
     </div>
   );
 }
---
+++ src/Button.jsx
@@ -0,0 +1,11 @@
+function Button({ onClick }) {
+  console.log(onClick);
+
+  return (
+    <button onClick={onClick} style={{ backgroundColor: "blue" }}>
+      Submit
+    </button>
+  );
+}
+
+export default Button;
