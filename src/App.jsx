dd7baae53bfda0fbcfa12a989c39f5445dfeda59
Florian <floriangeorges1992@gmail.com>
2025-01-03 13:16:43 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import "./App.css";
 
 function App() {
-  function handleClick() {
-    console.log("click !");
+  function handleClick(e) {
+    console.log("click ! : ", e);
+  }
+
+  function handleInput(e) {
+    console.log("input ! : ", e);
+  }
+
+  function handleFocus(e) {
+    console.log("focus ! : ", e);
   }
 
   return (
     <div className="d-flex flex-row justify-content-center align-items-center p-20">
-      <button onClick={handleClick}>Submit</button>
+      <button onClick={handleClick} className="mb-20">
+        Submit
+      </button>
+      <input onFocus={handleFocus} type="text" onInput={handleInput} />
     </div>
   );
 }
