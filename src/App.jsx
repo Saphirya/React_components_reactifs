3623f5b1b1061415e5a5d279ade0339bbf57615c
Florian <floriangeorges1992@gmail.com>
2025-01-04 22:17:52 +0100

    add batch

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,22 @@ function App() {
   console.log("render App");
 
   const [count, setCount] = useState(0);
+  const [showCount, setShowCount] = useState(true);
 
   function handleClick() {
     setCount(count + 1);
     console.log(count);
+
+    setShowCount(!showCount);
+    console.log(showCount);
   }
 
   return (
     <div className=" d-flex flex-column justify-content-center align-items-center p-20">
       <button onClick={handleClick} from="App">
-        Submit
+        Submit {showCount && <span> {count}</span>}
       </button>
-      <Compteur count={count} />
+      <Compteur />
     </div>
   );
 }
